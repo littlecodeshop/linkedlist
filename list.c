0999e8a77d4e5fadd1d95a9d7ae761ac5d4013a9
LittleCodeShop <richard@littlecodeshop.com>
2014-11-22 23:39:06 +0100

    adds dump_list

--- list.c
+++ list.c
@@ -42,13 +42,31 @@ void dump_element(list l,char *format){
     }
 }
 
+void dump_lists(list l,char *format){
+
+    node * ptr = l.head;
+    while(ptr){
+        printf("LIST :\n");
+        list * al = (list*)(ptr->element);
+        dump_element(*al,format);
+        ptr = ptr->next;
+    }
+}
 int main(int argc, char **argv){
     list * l = list_init();
+    list * l2 = list_init();
+    list * l3 = list_init();
+
     addElement(l,"hello");
     addElement(l,"world");
-    addElement(l,"this");
-    addElement(l,"is");
-    addElement(l,"Richard");
+    addElement(l2,"this");
+    addElement(l2,"is");
+    addElement(l2,"Richard");
+    addElement(l3,l);
+    addElement(l3,l2);
     dump_element(*l,"%s\n");
+    dump_element(*l2,"%s\n");
+    dump_lists(*l3,"%s\n");
+
 }
 
