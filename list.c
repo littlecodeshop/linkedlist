f8e4581a6704af5ef2230b44facbbe9856092571
LittleCodeShop <richard@littlecodeshop.com>
2014-11-22 21:15:48 +0100

    adding real code and makefile

--- list.c
+++ list.c
@@ -1,13 +1,54 @@
+
 #include <stdio.h>
 #include <stdlib.h>
 
-struct _node{
-    char * data;
+typedef struct _node{
+    int element;
     struct _node * next;
+} node;
+
+typedef struct {
+    node * head; //pointeur sur premier element
+    node * tail; //pointeur sur dernier element
+} list;
+
+
+list* list_init(){
+    list * l = (list*)malloc(sizeof(list));
+    l->head=l->tail=NULL;
+    return l;
 }
 
-int main(int argc, char ** argv)
-{
-
-}
+void addElement(list * l, int el){
     
+    //create the new Node
+    node * n = (node*)malloc(sizeof(node));
+    n->element = el;
+    n->next = NULL;
+    if(l->head == NULL){ //TODO : comment eviter ca ?
+        l->head = n;
+        l->tail = n;
+    } 
+    l->tail->next = n;
+    l->tail = n;
+}
+
+void dump(list l){
+
+    node * ptr = l.head;
+    while(ptr){
+        printf("=>%d\n",(ptr)->element);
+        ptr = ptr->next;
+    }
+}
+
+int main(int argc, char **argv){
+    list * l = list_init();
+    addElement(l,1);
+    addElement(l,2);
+    addElement(l,3);
+    addElement(l,4);
+    addElement(l,5);
+    dump(*l);
+}
+
